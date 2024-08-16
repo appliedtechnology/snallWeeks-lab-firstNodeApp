8808ecb81a03ca15b27381533c7a95513118f7ca
defycab <love.bellinder@gmail.com>
2024-08-17 00:32:52 +0200

    rewritting the solution to use fewer steps

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // prompt sync is a program added as a dependency
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")()
 
-function greet() {
-  return `Welcome to SALT, ${name}`;
+function greet(namedPassedIn) {
+  return "Welcome to SALT, " + namedPassedIn
 }
 
 // Main program
 // sparar det som skrivs i prompten som en variabel
-const name = prompt('What is your name? ');
-const greeting = greet(name);
-console.log(greeting);
+const name = prompt("What is your name? ")
+console.log(greet(name))
 
-module.exports.greet = greet;
+module.exports.greet = greet
